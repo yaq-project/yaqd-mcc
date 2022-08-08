ffd2e3850e910de4762b979c96f098a78e979900
pcruzparri <peter.cruzparrilla@gmail.com>
2022-08-08 13:29:23 -0500

    cleaned code and added async control to cancel previous set_position() tasks if a new one is called

--- yaqd_mcc/_mcc_152.py
+++ yaqd_mcc/_mcc_152.py
@@ -2,7 +2,7 @@ __all__ = ["Mcc152"]
 
 import asyncio
 from typing import Dict, Any, List
-import daqhats
+import daqhats # type: ignore
 from yaqd_core import IsDaemon, HasPosition, HasLimits
 
 
@@ -13,54 +13,24 @@ class Mcc152(HasLimits, HasPosition, IsDaemon):
         super().__init__(name, config, config_filepath)
         # Set up board communication address
         self.address = self._config["address"]
-        self.terminal = (self._config["terminal"][:-1], int(self._config["terminal"][-1]))
+        self.terminal = self._config["terminal"]
 
         # Set up initiation of board and outputs
-        self.busy = True
         self.d = daqhats.mcc152(self.address)
-        if self.terminal[0] == "ao" and 0 <= self.terminal[1] <= 1:
-            self.d.a_out_write(self.terminal[1], 0)
-        elif self.terminal[0] == "dio" and 0 <= self.terminal[1] <= 7:
-            pass  # implement dio reseting here
-        else:
-            pass  # implement error here
-        self._set_position(0)
-        self._state["destination"] = 0
-        self.busy = False
-        print(config_filepath)
-
         self._state["hw_limits"] = [0, 5]
-
-    async def update_state(self):
-        """Continually monitor and update the current daemon state."""
-        # If there is no state to monitor continuously, delete this function
-        while True:
-            # Perform any updates to internal state
-            self._busy = False
-            # There must be at least one `await` in this loop
-            # This one waits for something to trigger the "busy" state
-            # (Setting `self._busy = True)
-            # Otherwise, you can simply `await asyncio.sleep(0.01)`
-            # await self._busy_sig.wait()
-            await asyncio.sleep(0.01)
 
     def get_address(self):
         return self.address
 
-    def _set_position(self, v):  # why this method need _ before but doesnt show as so in messages?
+    def _set_position(self, v):
         """
         voltage is float 0.0-5.0
         """
-
-        if self.terminal[0] == "ao" and 0 <= self.terminal[1] <= 1:
-            try:
-                self.busy = True
-                self.d.a_out_write(self.terminal[1], float(v))
-                self._state["position"] = v
-                self.busy = False
-            except:
-                raise TypeError("The given voltage is not a number")
-        elif self.terminal[0] == "dio" and 0 <= self.terminal[1] <= 7:
-            pass  # implement digital io terminal writing
-        else:
-            pass  # implement appropriate error
+        async def _setter(self, v):
+            self.d.a_out_write(self.terminal, v)
+            self._state["position"] = v
+            await asyncio.sleep(5)
+            self._busy = False
+        for task in asyncio.all_tasks():
+            if task.get_name()=="setting position": task.cancel()
+        asyncio.create_task(_setter(self, v), name="setting position")
