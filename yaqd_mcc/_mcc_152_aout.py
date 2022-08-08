5c7ca269e24b0b29c168a646ae958a6c49eec6f1
pcruzparri <43578034+pcruzparri@users.noreply.github.com>
2022-08-08 14:18:34 -0500

    "updated naming convention for daemon to reflect this one only controls analog output terminals of the mcc152 daqhat"

--- yaqd_mcc/_mcc_152_aout.py
+++ yaqd_mcc/_mcc_152_aout.py
@@ -1,4 +1,4 @@
-__all__ = ["Mcc152"]
+__all__ = ["Mcc152AOut"]
 
 import asyncio
 from typing import Dict, Any, List
@@ -6,8 +6,8 @@ import daqhats  # type: ignore
 from yaqd_core import IsDaemon, HasPosition, HasLimits
 
 
-class Mcc152(HasLimits, HasPosition, IsDaemon):
-    _kind = "mcc-152"
+class Mcc152AOut(HasLimits, HasPosition, IsDaemon):
+    _kind = "mcc-152-aout"
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
