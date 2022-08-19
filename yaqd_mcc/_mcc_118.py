8d97129a30c764bdd289d38f745ce5f19a080685
Blaise Thompson <blaise@untzag.com>
2022-08-19 11:46:08 -0500

    mypy

--- yaqd_mcc/_mcc_118.py
+++ yaqd_mcc/_mcc_118.py
@@ -13,7 +13,7 @@ class Mcc118(HasMeasureTrigger, IsSensor, IsDaemon):
         super().__init__(name, config, config_filepath)
         self._channel_names = [f"ch{i}" for i in range(8)]
         self._channel_units = {k: "V" for k in self._channel_names}
-        import daqhats
+        import daqhats  # type: ignore
 
         self._hat = daqhats.mcc118()
 
